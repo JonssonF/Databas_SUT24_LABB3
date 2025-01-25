5d173da6cd5a986af46bfa382204911bff548c5c
Fredrik Jonsson <FredrikAndreJonson@hotmail.com>
2025-01-25 17:13:40 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace FREDRIK_JONSSON_SUT24_LABB3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = @"Data Source = localhost; Initial Catalog = LabbSchool; Integrated Security = true;";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+
+
+        }
+    }
+}
---
+++ SchoolDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FREDRIK_JONSSON_SUT24_LABB3
+{
+    public class SchoolDbContext : DbContext
+    {
+        //public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+
+    }
+}
